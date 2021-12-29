70b0cb0841dd19130f36db32813d06a8c7331ed0
James Deasley <deasleyj@tcd.ie>
2021-12-29 18:55:31 +0000

    Added implementation for creating dictionary from a user, as well as function to create a fake user and updating previous get_user function with error checking

--- get_data.py
+++ get_data.py
@@ -1,10 +1,46 @@
 from github import Github
 import os
 
+from faker import Faker
+from collections import defaultdict
+faker = Faker()
+fake = defaultdict(faker.name)
+
 token = os.getenv('GITHUB_PAT')
 g = Github(token)
 usr = None
 
 def get_user():
-    usr = g.get_user()
-    return g
+    try:
+        usr = g.get_user()
+    except Exception:
+        usr = None
+
+    return usr
+
+def get_fake_user(name):
+    try:
+        usr = g.get_user(name)
+    except Exception:
+        usr = None
+
+    return usr
+
+def create_user_dict(user):
+    try:
+        dct = {
+            'user':         fake[user.login].replace(" ",""),
+            'fullname':     fake[user.name],
+            'location':     user.location,
+            'company':      user.company,
+            'public_repos': user.public_repos
+        }
+
+        for k, v in dict(dct).items():
+            if v is None:
+                del dct[k]
+    
+    except Exception:
+        dct = None
+
+    return dct
