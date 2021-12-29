add2d930da2c38fc359e8cd4ca0904d50c78f013
James Deasley <deasleyj@tcd.ie>
2021-12-29 18:53:16 +0000

    Added test for creating dictionary from a user

--- get_data_test.py
+++ get_data_test.py
@@ -4,4 +4,18 @@ import get_data
 class test_get_data(unittest.TestCase):
 
     def test_user(self):
-        self.assertIsNotNone(get_data.get_user)
+        self.assertIsNotNone(get_data.get_user())
+
+    def test_user_dict(self):
+        usr1 = get_data.get_user()
+        usr2 = get_data.get_fake_user("not_a_real_user")
+
+        dct1 = get_data.create_user_dict(usr1)
+        dct2 = get_data.create_user_dict(usr2)
+
+        self.assertIsNotNone(dct1)
+        self.assertIsNone(dct2)
+
+        for _, v in dict(dct1).items():
+            self.assertIsNotNone(v)
+
