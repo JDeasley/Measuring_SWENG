b056ae20ccf4cf63af97bce0a7c55199d5a8a97e
James Deasley <deasleyj@tcd.ie>
2021-12-31 23:08:42 +0000

    Updated app,py to work with new functions and to allow access to the 'repo.html' page of the app

--- app.py
+++ app.py
@@ -1,22 +1,34 @@
 from flask import Flask, render_template
 import os
 import json
+import get_data, cleardb, read_data
 
 tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
 app = Flask(__name__, template_folder=tmpl_dir)
+repos_fetched = []
 
-def fetch_data():
-    f = open('data.json')
-    rdata = json.load(f)
+def fetch_data(repo_name):
+    if repo_name in repos_fetched:
+        print("Already fetched repo: ", repo_name)
+    else:
+        # 1. Try to fetch data from DB for this repo
+        print("Fetching repo: ", repo_name)
+        get_data.fetch(repo_name)
+        repos_fetched.append(repo_name)
 
+        # f = open('data.json')
+        # rdata = json.load(f)
+
+    rdata = read_data.db_fetch(repo_name)
     return rdata
 
 @app.route("/")
 def index():
+    cleardb.main()
     return render_template("index.html")
 
 @app.route("/repo/<path:repo_name>")
 def show_repo(repo_name):
-    data = fetch_data()
+    data = fetch_data(repo_name)
     first_user = data[0]["user"]
-    return render_template("test.html", **locals())
+    return render_template("repo.html", **locals())
