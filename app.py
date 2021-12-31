37080cde9eec82e2862580270979652e1ed83d66
James Deasley <deasleyj@tcd.ie>
2021-12-31 21:04:45 +0000

    Added app.py, the base file for the Flask app

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import os
+import json
+
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+app = Flask(__name__, template_folder=tmpl_dir)
+
+def get_data():
+    f = open('data.json')
+    rdata = json.load(f)
+
+    return rdata
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/repo/<path:repo_name>")
+def show_repo(repo_name):
+    data = get_data()
+    first_user = data[0]["user"]
+    return render_template("test.html", **locals())
